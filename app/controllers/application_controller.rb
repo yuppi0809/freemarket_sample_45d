cd4616e3dae72b3f0d0917153389da60d463bd79
Jun <ogawajunpei@JunnoMacBook-Pro.local>
2019-02-24 21:54:55 +0900

    Add action. Devise_controller permit added params.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day])
+  end
 end
