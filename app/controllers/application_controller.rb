caa2ca0718c7816dbabf307152c92dff94937435
Jun <ogawajunpei@JunnoMacBook-Pro.local>
2019-02-25 21:31:22 +0900

    Add permit params to devise_cotroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana,
+      :birth_year, :birth_month, :birth_day,
+        profile_attributes: [
+          :prefecture, :city, :address, :postal_code, :building_name,
+          :telephone_num, :delv_first_name, :delv_last_name,
+          :delv_first_name_kana, :delv_last_name_kana
+        ]
+      ])
   end
 end
