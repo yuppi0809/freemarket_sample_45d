356ecea9a39ef2e81f0d879d54510e1998b93981
Jun <ogawajunpei@JunnoMacBook-Pro.local>
2019-02-28 20:40:41 +0900

    Add column 'verify_sms' to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190225034920) do
+ActiveRecord::Schema.define(version: 20190228103538) do
 
   create_table "profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "profile",              limit: 65535
@@ -46,8 +46,10 @@ ActiveRecord::Schema.define(version: 20190225034920) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "verify_sms",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["verify_sms"], name: "index_users_on_verify_sms", unique: true, using: :btree
   end
 
   add_foreign_key "profiles", "users"
