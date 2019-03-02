3ce0048a37d902f0efaf0fc074f8e769174e1342
Jun <ogawajunpei@JunnoMacBook-Pro.local>
2019-03-02 11:54:09 +0900

    Add method to helper
    To get credit-card expiration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def get_expiration_month
+    month = []
+    1.upto(12) do |num|
+      num = '%02d' % num
+      month << num
+    end
+    return month
+  end
+
+  def get_expiration_year
+    current_year = Date.today.strftime('%y').to_i
+    year = [current_year]
+    10.times do
+      current_year += 1
+      year << current_year
+    end
+  return year
+  end
 end
