aca96115f956e053a397c28ec275f633828db1c6
Jun <ogawajunpei@JunnoMacBook-Pro.local>
2019-03-03 19:08:53 +0900

    Restore mistakenly deleted files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+
+  def new
+    render layout: 'sell-form'
+  end
+
+  def show
+  end
+
+  def confirm_purchase
+    render layout: false
+  end
+
 end
