be492aabc3c705ae7d5867c9f613730a2f468392
sora1 <shinnosuke960801@gmail.com>
2019-03-04 15:42:10 +0900

    add_description_and_category_id test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,5 +12,16 @@ describe Product do
       expect(product.errors[:name]).to include("can't be blank")
     end
 
+    it 'is invalid without description' do
+      product = build(:product, description: nil)
+      product.valid?
+      expect(product.errors[:description]).to include("can't be blank")
+    end
+
+    it 'is invalid without category_id' do
+      product = build(:product, category_id: nil)
+      product.valid?
+      expect(product.errors[:category_id]).to include("can't be blank")
+    end
   end
 end
