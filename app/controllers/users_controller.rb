7326110b029e18ffaff21d53db6cebf1d0091ffb
beginerbeginer190105 <miyabi6125@gmail.com>
2019-03-04 20:27:01 +0900

    edit app/controllers/users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersController < ApplicationController
   end
 
   def identification
+    # @user = User.find(params[:id])
+    # @address = Address.find_or_initialize_by(id: params[:id])
+    # @year = (1900..2019).to_a.freeze
+    # @month = (1..12).to_a.freeze
+    # @day = (1..31).to_a.freeze
   end
+
+  private
+  # def user_params
+  #   params.require(:user).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day)
+  # end
 
 end
