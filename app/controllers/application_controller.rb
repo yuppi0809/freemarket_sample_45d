99f494b047e167e85ef518622fd04f1a5fffc2f1
Jun <ogawajunpei@JunnoMacBook-Pro.local>
2019-03-04 14:35:01 +0900

    Add setting basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, if: :production?
 
   private
 
@@ -17,5 +18,15 @@ class ApplicationController < ActionController::Base
           :card_num, :expiration_month, :expiration_year, :security_code
         ]
       ])
+  end
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,14 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  RECAPTCHA_SITE_KEY: ENV["RECAPTCHA_SITE_KEY"],
+  RECAPTCHA_SECRET_KEY: ENV["RECAPTCHA_SECRET_KEY"],
+  BASIC_AUTH_USER: ENV["BASIC_AUTH_USER:"],
+  BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
+}
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
