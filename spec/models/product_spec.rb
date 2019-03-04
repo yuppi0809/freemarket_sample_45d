d9b0fc541ac49687e80628132e00f0603dff0b75
sora1 <shinnosuke960801@gmail.com>
2019-03-04 15:17:34 +0900

    test_for_name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,6 +6,11 @@ describe Product do
       expect(product).to be_valid
     end
 
+    it 'is invalid without a name' do
+      product = build(:product, name: nil)
+      product.valid?
+      expect(product.errors[:name]).to include("can't be blank")
     end
+
   end
 end
