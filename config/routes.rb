3936e9d7e87284acf81c753a37f89b8198c7bd7b
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-03-05 20:32:41 +0900

    change the routing
    destroyアクションのパスをusers/:user_id/credit_cards/:idからcredit_cards/:idに変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   root 'products#index'
 
   resources :users, only: :index do
-    resources :payments, path: 'credit_cards', only:[:index, :destroy] do
+    resources :payments, path: 'credit_cards', only:[:index, :destroy], shallow:true do
     end
   end
+
   resources :profiles, only: [:new, :create]
   resources :products, only: [:new, :show] do
     get 'confirm_purchase', on: :member
