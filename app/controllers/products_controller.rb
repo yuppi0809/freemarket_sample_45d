fd9563d625a8666b9cdead2b0751bb20f1c8a543
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-03-06 17:33:46 +0900

    change function "product_parameter" in products_controller.rb
    user_idが保存されるよう記述を追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,5 +21,6 @@ class ProductsController < ApplicationController
   private
   def product_parameter
     params.require(:product).permit(:name, :description, :category_id, :size, :product_status, :delivery_fee, :local, :lead_time, :price, :transaction_status, product_images_attributes: [:image])
+    params.require(:product).permit(:name, :description, :category_id, :size, :product_status, :delivery_fee, :local, :lead_time, :price, :brand, :transaction_status, product_images_attributes: [:image]).merge(user_id: current_user.id)
   end
 end
