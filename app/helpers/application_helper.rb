a652ef4d37591d51cd6f1e9b72eafffe7dc47f53
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-03-06 23:52:49 +0900

    declare method "display_price"
    値段に対して３桁ごとにカンマをつけ、円マークをつけるメソッドを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,6 +29,10 @@ module ApplicationHelper
   def mypage_elements_setting
     elements = ["プロフィール", "発送元・お届け先住所変更", "支払い方法", "メール/パスワード", "本人情報", "電話番号の確認", "ログアウト"]
   end
+  def display_price(price)
+    new_price = number_with_delimiter(price, :delimiter => ',')
+    return "¥ #{new_price}"
+  end
 end
 
 
