3980935ea1dcbee9dafa7f5387fd9e952c1d5f84
beginerbeginer190105 <miyabi6125@gmail.com>
2019-03-06 15:23:31 +0900

    edit db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,16 @@
-ActiveRecord::Schema.define(version: 20190302103555) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20190302103555) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -89,7 +100,6 @@ ActiveRecord::Schema.define(version: 20190302103555) do
   end
 
   add_foreign_key "payments", "users"
-
   add_foreign_key "product_images", "products"
   add_foreign_key "products", "categories"
   add_foreign_key "profiles", "users"
