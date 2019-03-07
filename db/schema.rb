42e18973bc9ac17b2e45eb1d7dc699b3119a843d
beginerbeginer190105 <miyabi6125@gmail.com>
2019-03-07 13:25:31 +0900

    edit db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190302103555) do
+ActiveRecord::Schema.define(version: 20190306071337) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20190302103555) do
     t.datetime "updated_at",       null: false
     t.index ["card_num"], name: "index_payments_on_card_num", unique: true, using: :btree
     t.index ["user_id"], name: "index_payments_on_user_id", using: :btree
+  end
+
+  create_table "prefectures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "product_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
