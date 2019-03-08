359c81782ff1b1d03251b9ca0aea51601fee7b9d
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-03-08 16:11:14 +0900

    test if it "renders the :show template"
    :showアクションで正しいページが表示されるかテスト

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -5,3 +5,13 @@ describe ProductsController, type: :controller do
   let(:user) {product.user}
   let(:category) {product.category}
 
+    describe 'GET #show' do
+      context "test the template and an intance variable holding one record" do
+
+        before do
+           get :show, params: {id: product}
+        end
+
+        it "renders the :show template" do
+          expect(response).to render_template :show
+        end
