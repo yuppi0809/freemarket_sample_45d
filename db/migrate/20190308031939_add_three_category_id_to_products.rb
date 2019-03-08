2cb59ef8105939b4b3aad6b177fb14bd56a96eaf
sora1 <shinnosuke960801@gmail.com>
2019-03-08 14:53:47 +0900

    fixed_migration_file

--- db/migrate/20190308031939_add_three_category_id_to_products.rb
+++
@@ -1,7 +0,0 @@
-class AddThreeCategoryIdToProducts < ActiveRecord::Migration[5.0]
-  def change
-    add_column :products, :first_category_id, :integer
-    add_column :products, :second_category_id, :integer
-    add_column :products, :third_category_id, :integer
-  end
-end
