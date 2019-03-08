f8b18fb4bee61166bbe11ae07f204946d8ae894f
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-03-08 15:53:42 +0900

    declare necessary variables in product_page_modal.js

--- app/assets/javascripts/product_page_modal.js
+++ app/assets/javascripts/product_page_modal.js
@@ -1,1 +1,7 @@
 $(document).on('turbolinks:load', function(){
+  var modalButton = $(".item-container__links__left .report");
+
+  var modal = $(".modal")
+
+  var cancelButton = $(".modal__window__bottom").find(".left")
+
