543592901144b07807ae19185dd789813f54f090
sora1 <shinnosuke960801@gmail.com>
2019-03-08 11:29:30 +0900

    Create_model_brand

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190306071337) do
+ActiveRecord::Schema.define(version: 20190308020659) do
+
+  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -30,11 +36,6 @@ ActiveRecord::Schema.define(version: 20190306071337) do
     t.datetime "updated_at",       null: false
     t.index ["card_num"], name: "index_payments_on_card_num", unique: true, using: :btree
     t.index ["user_id"], name: "index_payments_on_user_id", using: :btree
-  end
-
-  create_table "prefectures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "product_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
