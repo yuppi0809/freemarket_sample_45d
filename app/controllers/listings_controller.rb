226d3b4565c14029d62dc1bf4c79ef703d283cd0
beginerbeginer190105 <miyabi6125@gmail.com>
2019-03-10 20:15:54 +0900

    edit app/controllers/listings_controller.rb
    destroyメソッドはビューで使わないため
    インスタンス変数で書かない

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,16 +8,12 @@ class ListingsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-  def delete
-    # redirect_to controller: 'listings', action: 'index'
-    redirect_to user_listings
-  end
 
-  # def destroy
-  #   listings = listings.find(params[:id])
-  #   listings.destroy if listings.user_id == current_user.id
-  #   redirect_to user_listings
-  # end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+  end
 
 
   def update
