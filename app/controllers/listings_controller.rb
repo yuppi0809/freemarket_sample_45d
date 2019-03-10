dc44a30def7dbc3af9d03736108d6f8b9d71a946
beginerbeginer190105 <miyabi6125@gmail.com>
2019-03-10 16:25:24 +0900

    edit app/controllers/listings_controller.rb

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,25 @@
+class ListingsController < ApplicationController
+
+  def index
+    @product = Product.all
+  end
+
+  def show
+  end
+
+  def delete
+    # redirect_to controller: 'listings', action: 'index'
+    redirect_to user_listings
+  end
+
+  # def destroy
+  #   listings = listings.find(params[:id])
+  #   listings.destroy if listings.user_id == current_user.id
+  #   redirect_to user_listings
+  # end
+
+
+  def update
+  end
+
+end
