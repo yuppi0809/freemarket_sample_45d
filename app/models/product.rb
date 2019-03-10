a997115f78a51eb0499e3416cb3570f103eee17a
beginerbeginer190105 <miyabi6125@gmail.com>
2019-03-10 20:21:04 +0900

    edit app/models/product.rb
    商品にひもづく画像のレコード消すために
    プロダクトレコードを削除すると画像のレコードも削除できる。
    →dependent: :destroy

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   belongs_to :second_category, class_name: 'Category', foreign_key: 'second_category_id'
   belongs_to :third_category, class_name: 'Category', foreign_key: 'third_category_id'
   belongs_to :brand, foreign_key: 'brand_id', optional: true
-  has_many :product_images
+  has_many :product_images, dependent: :destroy
   accepts_nested_attributes_for :product_images
 
   # enum
