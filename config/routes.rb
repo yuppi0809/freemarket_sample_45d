862c6a67509e50d5fc13c6d7babb5d8edac6bb3f
beginerbeginer190105 <miyabi6125@gmail.com>
2019-03-11 17:50:44 +0900

    edit config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,11 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :new, :show] do
     resources :listings, only:[:index, :update, :show, :destroy], shallow:true do
     end
+  end
   resources :payments, only:[:index, :new, :create, :show, :destroy]
   resources :profiles, only: [:new, :create, :edit, :update]
   resources :products, only: [:new, :create, :show, :index] do
     get 'confirm_purchase', on: :member
   end
-
   resources :categories, only: :new
 end
