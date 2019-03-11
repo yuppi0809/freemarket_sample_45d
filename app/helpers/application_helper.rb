4da5f57e422fb7639dc2d8dfa5dfe28905fc29c2
Jun <ogawajunpei@JunnoMacBook-Pro.local>
2019-03-11 15:54:57 +0900

    Replace '#' with the each path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,7 +19,21 @@ module ApplicationHelper
   end
 
   def mypage_elements
-    elements = {'マイページ': '#', 'お知らせ': '#', 'やることリスト': '#', 'いいね！一覧': '#', '出品する': 'http://localhost:3000/products/new', '出品した商品 - 出品中': '#', '出品した商品 - 取引中': '#', '出品した商品 - 売却済み': '#', '購入した商品 - 過去の取引': '#', 'ニュース一覧': '#', '評価一覧': '#', 'ガイド': '#', 'お問い合わせ': '#'}
+    elements = {
+      'マイページ': '#',
+      'お知らせ': '#',
+      'やることリスト': '#',
+      'いいね！一覧': '#',
+      '出品する': new_product_path,
+      '出品した商品 - 出品中': '#',
+      '出品した商品 - 取引中': '#',
+      '出品した商品 - 売却済み': '#',
+      '購入した商品 - 過去の取引': '#',
+      'ニュース一覧': '#',
+      '評価一覧': '#',
+      'ガイド': '#',
+      'お問い合わせ': '#'
+    }
   end
 
   def mypage_elements_merpay
@@ -27,7 +41,15 @@ module ApplicationHelper
   end
 
   def mypage_elements_setting
-    elements = {"プロフィール": '#', "発送元・お届け先住所変更": '#', "支払い方法": "http://localhost:3000/users/#{current_user.id}/credit_cards", "メール/パスワード": '#', "本人情報": 'http://localhost:3000/users/mypage/identification', "電話番号の確認": '#', "ログアウト": '#'}
+    elements = {
+      "プロフィール": '#',
+      "発送元・お届け先住所変更": edit_profile_path(current_user.id),
+      "支払い方法": payment_path(current_user.id),
+      "メール/パスワード": '#',
+      "本人情報": user_path(current_user.id),
+      "電話番号の確認": '#',
+      "ログアウト": users_path
+    }
   end
 
   def registration_steps
