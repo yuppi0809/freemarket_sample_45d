8ee2f7c5b499254c19ada9b27b9e6dd6b1cda039
beginerbeginer190105 <miyabi6125@gmail.com>
2019-03-11 16:24:11 +0900

    edit app/controllers/listings_controller.rb
    リファクタリング
    before_action :set_profile, only: [:show, :destroy]
    
    show と destroy で処理をメソッドで定義
    before_actionで呼び出せるようにした。

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,23 +1,27 @@
 class ListingsController < ApplicationController
 
+  before_action :set_profile, only: [:show, :destroy]
+
   def index
     @product = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
-
-
   def destroy
-    product = Product.find(params[:id])
-    product.destroy
+    @product.destroy
     redirect_to action: :index, user_id: current_user.id
   end
 
 
   def update
+  end
+
+  private
+
+  def set_profile
+    @product = Product.find(params[:id])
   end
 
 end
