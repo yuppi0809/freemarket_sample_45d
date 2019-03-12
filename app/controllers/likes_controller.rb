065972eed9c770d9d3206a7c4bf541a1682a044f
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-03-12 19:32:03 +0900

    declare create action
    create.js.hamlで使うインスタス変数も定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,7 @@
 class LikesController < ApplicationController
+
+  def create
+    @like = Like.create(user_id: current_user.id, product_id: params[:product_id])
+    @product = Product.find(params[:product_id])
+    end
 end
