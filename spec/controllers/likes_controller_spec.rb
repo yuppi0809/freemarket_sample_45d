41e2065d9435f71fcd49d72e6c08fa9ee9a93fc4
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-03-13 17:53:50 +0900

    add variables and "before do login user"

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 describe LikesController, type: :controller do
+  let(:product){create(:product)}
+  let(:user){product.user}
+
+    before do
+      login user
+    end
+
   describe '#create' do
   describe '#destroy' do
   end
