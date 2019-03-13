a5ab9ca59f17369f525aed48a661b1aa92d0960a
yuppi0809 <yuppi@YukonoMacBook-ea.local>
2019-03-13 17:29:22 +0900

    add "before_action :authenticate_user!" in likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
 
+  before_action :authenticate_user!
   def create
     @like = Like.create(user_id: current_user.id, product_id: params[:product_id])
     @product = Product.find(params[:product_id])
