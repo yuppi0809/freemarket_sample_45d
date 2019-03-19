7e4a092bb62c3400e053dcbf5eed55ec755cbc79
Jun <ogawajunpei@JunnoMacBook-Pro.local>
2019-03-19 19:11:47 +0900

    Setting to use google_oauth2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -31,6 +31,9 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
+  config.omniauth :google_oauth2,
+                  ENV['GOOGLE_APP_ID'],
+                  ENV['GOOGLE_APP_SECRET']
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
